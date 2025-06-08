a996cdb82224f4dbe994d592208bf6b457fc5852
Gili <giliolig22@gmail.com>
2025-06-08 11:26:47 -0300

    feat: exibe lista de posts da API na rota /post

--- src/PostList.js
+++ src/PostList.js
@@ -1,1 +1,30 @@
- 
+ import { useEffect, useState } from "react";
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+function PostList() {
+  const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(data => setPosts(data));
+  }, []);
+
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>Lista de Posts</Typography>
+      <List>
+        {posts.map(post => (
+          <ListItem key={post.id} button onClick={() => navigate(`/dados/${post.id}`)}>
+            <ListItemText primary={post.title} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export default PostList;
+
