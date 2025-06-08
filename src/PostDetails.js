ac405442613cb2a2fe21fe53b0bb493398426419
Gili <giliolig22@gmail.com>
2025-06-08 11:28:24 -0300

    feat: mostra detalhes do post na rota /dados/:id

--- src/PostDetails.js
+++ src/PostDetails.js
@@ -1,1 +1,26 @@
- 
+ import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Typography } from "@mui/material";
+
+function PostDetails() {
+  const { id } = useParams();
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then(res => res.json())
+      .then(data => setPost(data));
+  }, [id]);
+
+  if (!post) return <div>Carregando...</div>;
+
+  return (
+    <div>
+      <Typography variant="h4">{post.title}</Typography>
+      <Typography variant="body1">{post.body}</Typography>
+    </div>
+  );
+}
+
+export default PostDetails;
+
